2aae71cca1e70807dac85c39ffd6ee9bf5ae00cd
Brian Wright <brwr@mit.edu>
2015-10-26 03:01:47 -0400

    Problem 1 calls function plf now

--- problem1.m
+++ problem1.m
@@ -1,10 +1,10 @@
+clf;clear;home;
+figure(1)
 u = [0 2 4 6 6 4 2 1 -1 -2 -4 -6 -6 -4 -2 0];
-ux = 0:1/length(u):1-1/length(u);
-stairs(ux,u)  % Stairstep plot
-grid on
-%disp(length(u));
-%disp(length(ux));
+plf(u);
+figure(2)
 w = [u u u u u u u u];
+plf(w);
+figure(3)
 w = [u u u u u u u u u];
-wx = 0:1/length(w):1-1/length(w);
-stairs(wx,w)
+plf(w);
