86bc7214a1a1c9f0a40abda8e94cffe4ce1c6eb6
Brian Wright <brwr@mit.edu>
2015-10-27 23:19:00 -0400

    haar_inv needed extra argument

--- haar_inv.m
+++ haar_inv.m
@@ -2,8 +2,8 @@ function [array] = haar_inv(coeffs)
     if size(coeffs, 2) == 1
         array = coeffs;
     else
-        half1 = haar_inv_step(coeffs(:, 1:end/2));
-        half2 = coeffs(:, end/2+1:end);
+        half1 = haar_inv_step(coeffs(:, 1:(end/2)),1);
+        half2 = coeffs(:, (end/2+1):end);
         array = kron(half1, [1,1]) + kron(half2, [1,-1]);
     end
 end
