6fdd6c026246a345e59f7d4792e4641c0894c05e
Brian Wright <brwr@mit.edu>
2015-10-27 23:31:20 -0400

    Problem 3 has plots and sound manipulation

--- haar_inv.m
+++ haar_inv.m
@@ -2,7 +2,7 @@ function [array] = haar_inv(coeffs)
     if size(coeffs, 2) == 1
         array = coeffs;
     else
-        half1 = haar_inv_step(coeffs(:, 1:(end/2)),1);
+        half1 = haar_inv_step(coeffs(:, 1:(end/2)),20);
         half2 = coeffs(:, (end/2+1):end);
         array = kron(half1, [1,1]) + kron(half2, [1,-1]);
     end
--- problem3.m
+++ problem3.m
@@ -25,9 +25,10 @@ title('Vector w after haar_step(k=6)')
 figure(7)
 plf(haar_step(w,7))
 title('Vector w after haar_step(k=7)')
- 
 load handel
+
 handel = y(1:65536);
+
 sound(handel);
 pause(8);
 c = haar_step(handel',1);
@@ -41,4 +42,11 @@ sound(c);
 pause(8);
 sound(haar_inv_step(c,3));
 pause(8);
+
 c = haar(handel');
+c1 = c;
+c1(32768:end)=0;
+sound(haar_inv(c1))
+pause(8)
+c1(1:2:16383)=0;
+sound(haar_inv(c1))
